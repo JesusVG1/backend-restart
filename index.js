52379eb00b30d13b4c4a3917c177c6aad491bdca
JesusVG1 <jesusvaladezguzman04@gmail.com>
2026-03-24 14:19:57 -0600

    Dia 1 backend restart

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API funcionando");
+});
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000")
+})
+
+app.get("/users", (req, res) => {
+    res.json ([
+        { id: 1, name: "Juan" },
+        { id: 2, name: "Maria" }
+    ]);
+});
