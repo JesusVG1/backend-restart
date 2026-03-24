1af262d014af998626cdcd45deff19316fab0039
JesusVG1 <jesusvaladezguzman04@gmail.com>
2026-03-24 15:44:53 -0600

    feat: basic Express API with users ednpoints

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
-
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("API funcionando");
+    res.send("ESTA ES LA VERSION NUEVA");
 });
-app.listen(3000, () => {
-    console.log("Servidor en puerto 3000")
-})
 
 app.get("/users", (req, res) => {
-    res.json ([
+    res.json([
         { id: 1, name: "Juan" },
         { id: 2, name: "Maria" }
     ]);
 });
+
+app.post("/users", (req, res) => {
+    const { name } =req.body;
+    
+    res.json({
+        message: "Usuario creado",
+        user: { id: Date.now(), name }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000")
+})
+
